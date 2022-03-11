3d7a0d638de2b74323da5aaa640bba9baec6305a
Ismael <http.ismaeel@gmail.com>
2022-03-11 20:14:40 -0300

    firebase crash lytics adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'core/presenter/app_widget.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await dotenv.load(fileName: 'assets/hasura_settings.env');
-  runApp(const AppWidget());
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    await dotenv.load(fileName: 'assets/hasura_settings.env');
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+    runApp(const AppWidget());
+  }, ((exception, stack) {
+    FirebaseCrashlytics.instance.recordError(exception, stack);
+  }));
 }
