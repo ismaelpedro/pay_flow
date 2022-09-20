a5d39253fe312d28bfa70b81f2fd5b7ae61e0c5d
Ismael Pedro <http.ismaeel@gmail.com>
2022-09-20 11:52:41 -0300

    app pub admob

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'firebase_options.dart';
 import 'src/modules/core/presenter/pay_flow_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  await Future.wait(<Future<Object?>>[
+    MobileAds.instance.initialize(),
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+  ]);
+
   runApp(const PayFlowApp());
 }
