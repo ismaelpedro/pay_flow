cf0e3e6abfe7802bc34ee42a08586e17452c7d44
Ismael Pedro <http.ismaeel@gmail.com>
2022-09-29 15:58:14 -0300

    icones e ad mob

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'firebase_options.dart';
 import 'src/modules/core/presenter/pay_flow_app.dart';
@@ -10,6 +11,7 @@ import 'src/modules/core/presenter/pay_flow_app.dart';
 Future<void> main() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await MobileAds.instance.initialize();
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
