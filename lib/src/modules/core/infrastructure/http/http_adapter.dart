9922620f4944a5c5fd10e3955aa2dc3e9c15abe7
Ismael Pedro <http.ismaeel@gmail.com>
2022-12-17 13:55:25 -0300

    trocado bloc por mobx, ajuste http adapter

--- lib/src/modules/core/infrastructure/http/http_adapter.dart
+++ lib/src/modules/core/infrastructure/http/http_adapter.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
 
 import 'package:dio/dio.dart';
 
@@ -10,7 +12,6 @@ class HttpAdapter implements HttpClient {
   final String baseUrl;
   final Map<String, String> headers;
   final List<HttpInterceptor> interceptors = <HttpInterceptor>[];
-
   HttpAdapter({
     required this.client,
     required this.baseUrl,
@@ -24,90 +25,97 @@ class HttpAdapter implements HttpClient {
     'accept': 'application/json',
   };
 
+  String _prettyJson(dynamic json) {
+    
+    const JsonEncoder encoder = JsonEncoder.withIndent(' ');
+    return encoder.convert(json);
+  }
+
   Future<HttpResponse> _handleRequest(HttpOptions httpOptions) async {
     late Response<dynamic> response;
     final String url = baseUrl + httpOptions.path;
 
-    switch (httpOptions.method) {
-      case HttpMethod.get:
-        response = await client.get(
-          url,
-          queryParameters: httpOptions.query ?? <String, String>{},
-          options: Options(
-            headers: httpOptions.headers ?? _defaultHeaders,
-            sendTimeout: _defaultConnectionTimeout.inSeconds,
-          ),
-        );
-        break;
-      case HttpMethod.post:
-        response = await client.post(
-          url,
-          data: httpOptions.data,
-          queryParameters: httpOptions.query ?? <String, String>{},
-          options: Options(
-            headers: httpOptions.headers ?? _defaultHeaders,
-            sendTimeout: _defaultConnectionTimeout.inSeconds,
-          ),
-        );
-        break;
-      case HttpMethod.put:
-        response = await client.put(
-          url,
-          data: httpOptions.data,
-          queryParameters: httpOptions.query ?? <String, String>{},
-          options: Options(
-            headers: httpOptions.headers ?? _defaultHeaders,
-            sendTimeout: _defaultConnectionTimeout.inSeconds,
-          ),
-        );
-        break;
-      case HttpMethod.patch:
-        response = await client.patch(
-          url,
-          data: httpOptions.data,
-          queryParameters: httpOptions.query ?? <String, String>{},
-          options: Options(
-            headers: httpOptions.headers ?? _defaultHeaders,
-            sendTimeout: _defaultConnectionTimeout.inSeconds,
-          ),
-        );
-        break;
-      case HttpMethod.delete:
-        response = await client.delete(
-          url,
-          data: httpOptions.data,
-          queryParameters: httpOptions.query ?? <String, String>{},
-          options: Options(
-            headers: httpOptions.headers ?? _defaultHeaders,
-            sendTimeout: _defaultConnectionTimeout.inSeconds,
-          ),
-        );
-        break;
+    try {
+      switch (httpOptions.method) {
+        case HttpMethod.get:
+          response = await client.get(
+            url,
+            queryParameters: httpOptions.query ?? <String, String>{},
+            options: Options(
+              headers: httpOptions.headers ?? _defaultHeaders,
+              sendTimeout: _defaultConnectionTimeout.inSeconds,
+            ),
+          );
+          break;
+        case HttpMethod.post:
+          response = await client.post(
+            url,
+            data: httpOptions.data,
+            queryParameters: httpOptions.query ?? <String, String>{},
+            options: Options(
+              headers: httpOptions.headers ?? _defaultHeaders,
+              sendTimeout: _defaultConnectionTimeout.inSeconds,
+            ),
+          );
+          break;
+        case HttpMethod.put:
+          response = await client.put(
+            url,
+            data: httpOptions.data,
+            queryParameters: httpOptions.query ?? <String, String>{},
+            options: Options(
+              headers: httpOptions.headers ?? _defaultHeaders,
+              sendTimeout: _defaultConnectionTimeout.inSeconds,
+            ),
+          );
+          break;
+        case HttpMethod.patch:
+          response = await client.patch(
+            url,
+            data: httpOptions.data,
+            queryParameters: httpOptions.query ?? <String, String>{},
+            options: Options(
+              headers: httpOptions.headers ?? _defaultHeaders,
+              sendTimeout: _defaultConnectionTimeout.inSeconds,
+            ),
+          );
+          break;
+        case HttpMethod.delete:
+          response = await client.delete(
+            url,
+            data: httpOptions.data,
+            queryParameters: httpOptions.query ?? <String, String>{},
+            options: Options(
+              headers: httpOptions.headers ?? _defaultHeaders,
+              sendTimeout: _defaultConnectionTimeout.inSeconds,
+            ),
+          );
+          break;
+      }
+
+      return HttpResponse(
+        data: response.data,
+        status: response.statusCode!.convertToHttpStatus(),
+      );
+    } on DioError catch (e) {
+      return HttpResponse(
+        data: e.response?.data,
+        status: e.response?.statusCode!.convertToHttpStatus() ?? HttpStatus.internalServerError,
+      );
+    } finally {
+      log('===========================================================');
+      log('\nREQUEST METHOD: ${httpOptions.method.name.toUpperCase()} in $url');
+      if (httpOptions.data != null) {
+        log('\nREQUEST BODY: ${_prettyJson(httpOptions.data)}');
+      }
+      if (httpOptions.query != null) {
+        log('\nREQUEST QUERY PARAMS: ${_prettyJson(httpOptions.query)}');
+      }
+      if (httpOptions.headers != null) {
+        log('\nREQUEST HEADERS: ${_prettyJson(httpOptions.headers)}');
+      }
+      log('===========================================================');
     }
-
-    return HttpResponse(
-      data: response.data,
-      status: response.statusCode!.convertToHttpStatus(),
-    );
-  }
-
-  @override
-  Future<HttpResponse> delete(
-    String url, {
-    Map<String, String>? headers,
-    Map<String, dynamic>? query,
-    Map<String, dynamic>? body,
-    Duration? timeout = _defaultConnectionTimeout,
-  }) async {
-    return _handleRequest(
-      HttpOptions(
-        path: url,
-        method: HttpMethod.delete,
-        data: body,
-        headers: headers,
-        timeout: timeout,
-      ),
-    );
   }
 
   @override
@@ -122,28 +130,8 @@ class HttpAdapter implements HttpClient {
         path: url,
         method: HttpMethod.get,
         headers: headers,
-        timeout: timeout,
         query: query,
-      ),
-    );
-  }
-
-  @override
-  Future<HttpResponse> patch(
-    String url, {
-    Map<String, String>? headers,
-    Map<String, String>? query,
-    Map<String, dynamic>? body,
-    Duration? timeout = _defaultConnectionTimeout,
-  }) async {
-    return _handleRequest(
-      HttpOptions(
-        path: url,
-        method: HttpMethod.patch,
-        data: body,
-        headers: headers,
         timeout: timeout,
-        query: query,
       ),
     );
   }
@@ -184,6 +172,46 @@ class HttpAdapter implements HttpClient {
         headers: headers,
         timeout: timeout,
         query: query,
+      ),
+    );
+  }
+
+  @override
+  Future<HttpResponse> patch(
+    String url, {
+    Map<String, String>? headers,
+    Map<String, String>? query,
+    Map<String, dynamic>? body,
+    Duration? timeout = _defaultConnectionTimeout,
+  }) async {
+    return _handleRequest(
+      HttpOptions(
+        path: url,
+        method: HttpMethod.patch,
+        data: body,
+        headers: headers,
+        timeout: timeout,
+        query: query,
+      ),
+    );
+  }
+
+  @override
+  Future<HttpResponse> delete(
+    String url, {
+    Map<String, String>? headers,
+    Map<String, String>? query,
+    Map<String, dynamic>? body,
+    Duration? timeout = _defaultConnectionTimeout,
+  }) async {
+    return _handleRequest(
+      HttpOptions(
+        path: url,
+        method: HttpMethod.delete,
+        data: body,
+        headers: headers,
+        query: query,
+        timeout: timeout,
       ),
     );
   }
--- lib/src/modules/core/infrastructure/service_locator/service_locator.dart
+++ lib/src/modules/core/infrastructure/service_locator/service_locator.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -9,14 +10,20 @@ import '../../core.dart';
 import '../../services/firebase_messaging_service.dart';
 import '../../services/notification_service.dart';
 import '../google_sign_in/google_sign_in_adapter.dart';
+import '../http/http.dart';
 
 final GetIt serviceLocator = GetIt.instance;
 
 void setUpInjections() {
   ///[Packages]
+  /// External Packages
   serviceLocator.registerSingleton(GoogleSignIn());
-  serviceLocator.registerSingleton(NotificationService());
-  serviceLocator.registerSingleton(FirebaseMessagingService(serviceLocator.get()));
+  serviceLocator.registerSingleton(
+    HttpAdapter(
+      baseUrl: 'https://localhost',
+      client: Dio(),
+    )..addInterceptors(<HttpInterceptor>[]),
+  );
 
   /// [DataSources]
   /// Consist of remote and local Data Sources. Remote Data Source will perform HTTP requests on the API. While local Data Source will cache or persist data.
@@ -26,16 +33,18 @@ void setUpInjections() {
 
   /// [Adapters]
   serviceLocator.registerSingleton(GoogleSignInAdapter(serviceLocator.get()));
+  serviceLocator.registerSingleton(NotificationService());
+  serviceLocator.registerSingleton(FirebaseMessagingService(serviceLocator.get()));
 
   /// [Usecases]
   /// Application-specific business rules
-  serviceLocator
-      .registerSingleton(LoginWithGoogleUsecase(serviceLocator.get()));
+  serviceLocator.registerSingleton(LoginWithGoogleUsecase(serviceLocator.get()));
 
-  // Controllers
+  /// [Controllers]
+  /// Controllers are a means to give control to the parent widget over its child state.
   serviceLocator.registerSingleton(LoginController(serviceLocator.get()));
   serviceLocator.registerSingleton(HomeController());
   serviceLocator.registerSingleton(AppController());
 
-  debugPrint('Set Up All Injections!');
+  debugPrint('-- Set Up All Injections!');
 }
