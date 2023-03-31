cc328b9739e683e427fbf8162fae645e678de5d3
Ismael Pedro <http.ismaeel@gmail.com>
2023-03-31 12:50:23 -0300

    ajustes

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-# Get current version from pubspec.yaml
 VERSION=$(grep 'version: ' pubspec.yaml | sed 's/version: //')
 
-# Extract major, minor, and patch version numbers
 MAJOR=$(echo "$VERSION" | cut -d '.' -f 1)
 MINOR=$(echo "$VERSION" | cut -d '.' -f 2)
 PATCH=$(echo "$VERSION" | cut -d '.' -f 3 | cut -d '+' -f 1)
 
-# Increment patch version
 PATCH=$((PATCH+1))
-BUILD_NUMBER=${FCI_BUILD_NUMBER:-1}
+LAST_COMMIT=$(git log --format="%H" -n 1)
+FCI_BUILD_NUMBER=$(git rev-list --count $LAST_COMMIT)
+FCI_BUILD_NUMBER=$((FCI_BUILD_NUMBER+1))
 
-# Create new version number with incremented patch
-NEW_VERSION="$MAJOR.$MINOR.$PATCH+$BUILD_NUMBER"
+NEW_VERSION="$MAJOR.$MINOR.$PATCH+$FCI_BUILD_NUMBER"
 echo "$NEW_VERSION"
 
-# Replace version in pubspec.yaml with new version number
 if sed -i.bak "s/version: $VERSION/version: $NEW_VERSION/g" pubspec.yaml && rm pubspec.yaml.bak; then
   echo "Version in pubspec.yaml updated successfully"
 else
@@ -24,7 +21,6 @@ else
   exit 1
 fi
 
-# Commit changes to git
 # if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]" && git push; then
 if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]"; then
   echo "Changes committed and pushed"
