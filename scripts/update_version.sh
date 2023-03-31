422d6fa28a401cbb190252e7675f3683edf141a3
Ismael Pedro <http.ismaeel@gmail.com>
2023-03-30 22:28:07 -0300

    adjustments

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Obtém a versão atual do aplicativo
+version=$(cat pubspec.yaml | grep -o '^[ ]*version:.*' | awk '{print $2}')
+
+# Remove o sinal de "+" da versão, se presente
+version=$(echo $version | sed 's/\+//g')
+
 # Incrementa o valor de build em 1
 build_number=$(expr ${FCI_BUILD_NUMBER} + 1)
 
 # Atualiza a versão no arquivo pubspec.yaml
-sed -i '' "s/version: .*/version: $(cat pubspec.yaml | grep -o '^[ ]*version:.*' | awk '{print $2}')+$build_number/" pubspec.yaml
+sed -i '' "s/version: .*/version: $version+$build_number/" pubspec.yaml
