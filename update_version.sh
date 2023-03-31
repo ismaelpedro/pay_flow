8f296ccb8300130a63854ac6862a202a3fc93ce3
Ismael Pedro <http.ismaeel@gmail.com>
2023-03-31 12:24:55 -0300

    ajustes

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Load variables from codemagic.yaml
-source "codemagic.yaml"
+source "$(dirname "$0")/../codemagic.yaml"
 
 # Get current version from pubspec.yaml
 VERSION=$(grep 'version: ' pubspec.yaml | sed 's/version: //')
@@ -19,8 +19,8 @@ NEW_VERSION="$MAJOR.$MINOR.$PATCH+${FCI_BUILD_NUMBER:-1}"
 echo "$NEW_VERSION"
 
 # Replace version in pubspec.yaml with new version number
-if sed -i.bak "s/version: $VERSION/version: $NEW_VERSION/g" pubspec.yaml; then
-  rm pubspec.yaml.bak
+if sed -i.bak "s/version: $VERSION/version: $NEW_VERSION/g" pubspec.yaml && rm pubspec.yaml.bak; then
+  echo "Version in pubspec.yaml updated successfully"
 else
   echo "Failed to update version in pubspec.yaml"
   exit 1
@@ -28,7 +28,7 @@ fi
 
 # Commit changes to git
 if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]" && git push; then
-  echo "Changes committed and pushed to GitHub"
+  echo "Changes committed and pushed"
 else
   echo "Failed to commit and push changes to GitHub"
   exit 1
