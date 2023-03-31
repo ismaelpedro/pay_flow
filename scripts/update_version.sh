a71a62e4e1ebf7007332d5c6164ec92f2d24457a
Ismael Pedro <http.ismaeel@gmail.com>
2023-03-30 22:56:51 -0300

    adjustments

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -10,8 +10,8 @@ PATCH=$(echo $VERSION | awk -F '.' '{print $3}' | awk -F '+' '{print $1}')
 
 # Incrementa o número de build em 1
 FCI_BUILD_NUMBER=$(($FCI_BUILD_NUMBER + 1))
-NEW_VERSION = $MAJOR.$MINOR.$PATCH+${FCI_BUILD_NUMBER}
-echo NEW_VERSION
+NEW_VERSION=$MAJOR.$MINOR.$PATCH+${FCI_BUILD_NUMBER}
+echo $NEW_VERSION
 
 # Atualiza a versão no arquivo pubspec.yaml
 sed -i '' "s/version: $VERSION/version: $NEW_VERSION/g" pubspec.yaml
