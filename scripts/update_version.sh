48609a46d91124cbef8a2aad7e1e8c77750568a9
Ismael Pedro <http.ismaeel@gmail.com>
2023-04-06 16:46:43 -0300

    ajuste

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -21,7 +21,7 @@ else
   exit 1
 fi
 
-if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]" && git push origin main --force; then
+if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]" && git push origin dev --force; then
   echo "Changes committed and pushed"
 else
   echo "Failed to commit and push changes"
