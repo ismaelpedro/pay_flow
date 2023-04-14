86de245d71237f3f76e8cda9f0ac146a10ce41d2
Ismael Pedro <http.ismaeel@gmail.com>
2023-04-14 00:57:58 -0300

    AJUSTES SCRIPT AUTO INCREMENT

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -21,7 +21,7 @@ else
   exit 1
 fi
 
-if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]" && git status && git push origin dev --force; then
+if git add . && git commit -m "chore: Increment version to $NEW_VERSION [SCRIPT]" && git push https://ismaelpedro:$APP_PASSWORD@github.com/ismaelpedro/pay_flow.git; then
   echo "Changes committed and pushed"
 else
   echo "Failed to commit and push changes"
